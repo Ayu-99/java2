b3ce7a480ebcf986723d0d487df1392537b70123
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2019-06-04 19:13:32 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+
+public class App {
+	//main represents main thread of process
+	//whatever we write in main will be execute in a sequence by main thread of process
+	public static void main(String[] args) {
+		//read statements to display information
+		System.out.println("task1");
+		System.out.println("task2");
+		System.out.println("task3");
+		
+		
+
+	}
+
+}
